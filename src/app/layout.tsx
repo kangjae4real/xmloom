013611343992b6ef80ec9ec2fb7f4ff74c7c9fec
kangjae4real <awpe89@gmail.com>
2026-05-03 18:09:55 +0900

    chore: install shdcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next';
+import { Geist as GeistFont } from 'next/font/google';
+import { cn } from '@/lib/utils';
 import './globals.css';
+
+const geistFont = GeistFont({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'XMLoom',
@@ -11,8 +15,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const geistFontClassName = cn('font-sans', geistFont.variable);
+
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className={cn('antialiased', geistFontClassName)}>
       <body>{children}</body>
     </html>
   );
